29912f31111044a4614beebe5934d96da29df249
Attila Kanto <akanto@users.noreply.github.com>
2025-03-21 18:22:08 +0100

    Refactor device selection logic for to make it work with cuda as well, and add a linear schedule for learning_rate

--- src/train.py
+++ src/train.py
@@ -8,6 +8,15 @@ from environment import create_training_env
 import torch
 import location
 
+# On some systems, e.g MacOS, the default device is 'cpu' but we can use 'mps' for better performance https://developer.apple.com/metal/pytorch/
+def get_torch_device() -> torch.device:
+    if torch.cuda.is_available():
+        return torch.device("cuda")
+    elif torch.backends.mps.is_available():
+        return torch.device("mps")
+    else:
+        return torch.device("cpu")
+
 def linear_schedule(initial_value: float) -> Schedule:
     def func(progress_remaining: float) -> float:
         return progress_remaining * initial_value
@@ -17,15 +26,14 @@ def train():
 
     ensure_directory_exists(location.MODEL_DIR)
     ensure_directory_exists(location.LOG_DIR)
-
-    # On some systems, e.g MacOS, the default device is 'cpu' but we can use 'mps' for better performance https://developer.apple.com/metal/pytorch/
-    device = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
+    
+    device = get_torch_device()
     print(f"Training on device: {device}")
 
     env = create_training_env()
 
-    model = PPO('CnnPolicy', env, verbose=1, tensorboard_log=location.LOG_DIR, learning_rate=0.00001, n_steps=512, device=device)
-    model.learn(total_timesteps=1000000)
+    model = PPO('CnnPolicy', env, verbose=1, tensorboard_log=location.LOG_DIR, learning_rate=linear_schedule(0.00001), n_steps=512, device=device)
+    model.learn(total_timesteps=10_000_000)
     model.save(location.MODEL_PATH)
 
     print(f"Training complete, model saved to {location.MODEL_PATH}")
